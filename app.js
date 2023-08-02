b1992f353af332e0134b0a591b780d54e6f5a99c
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-02 20:27:56 +0530

    create server and display name

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+// const http = require('http');
+// const server=http.createServer((req,res)=>{
+//     console.log(req);
+//     console.log('anand naik')
+//     process.exit();
+// });
+
+// server.listen(4000);
 const http = require('http');
-const server=http.createServer((req,res)=>{
-    console.log(req);
-    console.log('anand naik')
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Anand Naik');
+  response.end();
 });
 
-server.listen(4000);
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});

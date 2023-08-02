18f065fc7d6fb967d2a9f63bdd8ba97dfc01c493
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-02 22:01:11 +0530

    creating pages and getting response

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const { text } = require('stream/consumers');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Anand Naik');
-  response.end();
+console.log(request.url,request.method,request.headers)
+response.setHeader('Content-Type','text')
+if(request.url==='/home'){
+    response.write('Welcome home')
+}else if(request.url==='/about'){
+    response.write('Welcome to About Us page')
+}else if(request.url==='/node'){
+    response.write('Welcome to my Nodejs Project')
+}
+response.end();
 });
 
 server.listen(4000, () => {
